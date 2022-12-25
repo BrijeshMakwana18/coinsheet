69b15afa44e9a5634ed8c400ec17c0f69e329f38
BrijeshMakwana18 <bamakwana2000@gmail.com>
2022-12-25 12:45:11 +0530

    global methods added

---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+import {
+  removeListeners,
+  getDisplayDate,
+  getCurrentMonth,
+  getCurrentTimestamps,
+} from './globalMethods';
+export {removeListeners, getDisplayDate, getCurrentMonth, getCurrentTimestamps};
