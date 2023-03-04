766c70ee2a9e8827a1bcf108cec00426f6f3b821
BrijeshMakwana18 <bamakwana2000@gmail.com>
2023-03-04 23:55:05 +0530

    basic circle progress added

--- client/src/screens/Statistics/index.js
+++ client/src/screens/Statistics/index.js
@@ -1,10 +1,12 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import {
   Text,
   View,
   StatusBar,
   ScrollView,
   TouchableOpacity,
+  TextInput,
+  SafeAreaView,
 } from 'react-native';
 import {connect} from 'react-redux';
 import {StatSkeleton} from '../../components';
@@ -18,26 +20,76 @@ import Animated, {
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated';
-class Statistics extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  // const strokeOffset = useSharedValue(radius * Math.PI * 2);
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+const AnimatedText = Animated.createAnimatedComponent(TextInput);
+const radius = 45;
+const circumference = radius * Math.PI * 2;
+const duration = 3000;
+const Statistics = () => {
+  const strokeOffset = useSharedValue(circumference);
 
-  // const animatedCircleProps = useAnimatedProps(() => {
-  //   return {
-  //     strokeDashoffset: withTiming(strokeOffset.value, {duration: 2000}),
-  //   };
-  // });
+  const percentage = useDerivedValue(() => {
+    const number = ((circumference - strokeOffset.value) / circumference) * 100;
 
-  // useEffect(() => {
-  //   strokeOffset.value = 0;
-  // }, []);
-  render() {
-    return <View style={styles.container}></View>;
-  }
-}
+    return withTiming(number, {duration: duration});
+  });
+
+  const strokeColor = useDerivedValue(() => {
+    return interpolateColor(
+      percentage.value,
+      [0, 50, 100],
+      ['rgb(246, 79, 89)', 'rgb(246, 246, 89)', 'rgb(79, 246, 89)'],
+    );
+  });
+
+  const animatedCircleProps = useAnimatedProps(() => {
+    return {
+      strokeDashoffset: withTiming(strokeOffset.value, {duration: duration}),
+      stroke: strokeColor.value,
+    };
+  });
+
+  const animatedTextProps = useAnimatedProps(() => {
+    return {
+      text: `${Math.round(percentage.value)}%`,
+    };
+  });
+
+  useEffect(() => {
+    strokeOffset.value = 0;
+  }, [strokeOffset]);
+  return (
+    <View
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+        backgroundColor: '#17021A',
+      }}>
+      <AnimatedText
+        style={{
+          color: 'white',
+          fontSize: 24,
+          fontWeight: 'bold',
+          position: 'absolute',
+        }}
+        animatedProps={animatedTextProps}
+      />
+      <Svg height="50%" width="50%" viewBox="0 0 100 100">
+        <AnimatedCircle
+          animatedProps={animatedCircleProps}
+          cx="50"
+          cy="50"
+          r="45"
+          stroke="rgb(246, 79, 89)"
+          strokeWidth="5"
+          fill="rgba(255,255,255,0.2)"
+          strokeDasharray={`${radius * Math.PI * 2}`}
+        />
+      </Svg>
+    </View>
+  );
+};
 
 const mapStateToProps = state => {
   return {LoginReducer: state.LoginReducer, AppReducer: state.AppReducer};
