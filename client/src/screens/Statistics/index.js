aaf48338c91138c8992a0fa0cab1da2af53e43b8
BrijeshMakwana18 <bamakwana2000@gmail.com>
2023-03-10 23:51:04 +0530

    animated circle added

--- client/src/screens/Statistics/index.js
+++ client/src/screens/Statistics/index.js
@@ -7,6 +7,8 @@ import {
   TouchableOpacity,
   TextInput,
   SafeAreaView,
+  Dimensions,
+  useCallback,
 } from 'react-native';
 import {connect} from 'react-redux';
 import {StatSkeleton} from '../../components';
@@ -20,15 +22,85 @@ import Animated, {
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated';
+
+const BACKGROUND_COLOR = '#444B6F';
+const BACKGROUND_STROKE_COLOR = '#303858';
+const STROKE_COLOR = '#A6E1FA';
+
+const {width, height} = Dimensions.get('window');
+
+const CIRCLE_LENGTH = 500; // 2PI*R
+const R = CIRCLE_LENGTH / (2 * Math.PI);
+
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 const Statistics = props => {
+  const needs = useSharedValue(0);
+  const wants = useSharedValue(0);
+  const investments = useSharedValue(0);
+  const savings = useSharedValue(0);
+  const {dashboardData} = props.AppReducer;
+  const totalIncome = dashboardData.totalIncome;
+  const needsPercent = dashboardData.stat.needs.total / totalIncome;
+  const wantsPercent = dashboardData.stat.wants.total / totalIncome;
+  const investmentsPercent = dashboardData.stat.investments.total / totalIncome;
+  const savingsPercent = dashboardData.stat.savings.total / totalIncome;
+  const animatedProps = useAnimatedProps(() => ({
+    strokeDashoffset: CIRCLE_LENGTH * (1 - needs.value),
+  }));
+
+  const progressText = useDerivedValue(() => {
+    return `${Math.floor(needs.value * 100)}`;
+  });
+
   useEffect(() => {
-    // strokeOffset.value = 0;
+    needs.value = withTiming(needsPercent, {
+      duration: 2000,
+    });
+    wants.value = withTiming(wantsPercent, {
+      duration: 2000,
+    });
+    investments.value = withTiming(investmentsPercent, {
+      duration: 2000,
+    });
+    savings.value = withTiming(savingsPercent, {
+      duration: 2000,
+    });
   }, []);
 
   return (
-    <View style={styles.container}>
-      <></>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.itemsContainer}>
+        <Svg
+          width={width / 2}
+          style={{
+            backgroundColor: 'green',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+          }}>
+          <Text>{`Needs\n${(needsPercent * 100).toFixed(2)}%`}</Text>
+          <Circle
+            cx={width / 4}
+            cy={height / 4}
+            r={R}
+            stroke={BACKGROUND_STROKE_COLOR}
+            strokeWidth={25}
+          />
+          <AnimatedCircle
+            cx={width / 4}
+            cy={height / 4}
+            r={R}
+            stroke={STROKE_COLOR}
+            strokeWidth={15}
+            strokeDasharray={CIRCLE_LENGTH}
+            animatedProps={animatedProps}
+            strokeLinecap={'round'}
+          />
+        </Svg>
+      </View>
+      <View style={styles.itemsContainer}></View>
+    </SafeAreaView>
   );
 };
 
--- client/src/screens/Statistics/styles.js
+++ client/src/screens/Statistics/styles.js
@@ -4,9 +4,14 @@ import {colors, perfectSize, fonts} from '../../theme';
 
 export default styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     backgroundColor: colors.primaryBackgroundColor,
     paddingTop: perfectSize(Platform.OS == 'ios' ? 56 : 40),
     padding: perfectSize(23),
+  },
+  itemsContainer: {
+    flex: 1,
+    width: '100%',
+    flexDirection: 'row',
   },
 });
